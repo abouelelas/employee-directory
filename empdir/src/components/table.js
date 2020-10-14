613e5b992ba2c8375173efe0172f774a6c1d6811
Sarah Abouelela <abouelelas@gmail.com>
2020-10-14 13:27:13 -0400

    fixing rendering errors

--- empdir/src/components/table.js
+++ empdir/src/components/table.js
@@ -1,37 +1,37 @@
 import React from 'react';
 
 
-function tableSetUp(props){
+function tableSetUp(props) {
     console.log(props.employees)
-return (
-    <SearchForm
-            handleFormSubmit={this.handleFormSubmit}
-            handleInputChange={this.handleInputChange}
-            name={this.state.name}
-          />
-    <table className="table table-hover">
-  <thead>
- 
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Image</th>
-      <th scope="col">Name</th>
-      <th scope="col">Phone</th>
-      <th scope="col">Email</th>
-      <th scope="col">Location</th>
-    </tr>
-  </thead>
-  <tbody>
-    
-  <tr>  {props.employees( (ID, i) => <Item info={ID} key={"item-" + i} />)}
-</tr>
-          
-    
-  </tbody>
-</table>
+    return (
+        <>SearchForm
+    handleFormSubmit={this.handleFormSubmit}
+    handleInputChange={this.handleInputChange}
+    name={this.state.name}
+
+            <table className="table table-hover">
+                <thead>
+
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Location</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {props.employees.map((ID, i) => 
+                render (<tr><Item info={ID} key={"item-" + i} /></tr>))}
 
 
-)
+                </tbody>
+            </table>
+        </>
+
+    )
 }
 
 
